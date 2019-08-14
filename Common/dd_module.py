3e11c5e5f177b97e1e33619c40f2210563881cc4
xuchen <357072695@qq.com>
2019-08-14 11:20:18 +0800

    修改钉钉配置

--- Common/dd_module.py
+++ Common/dd_module.py
@@ -7,12 +7,21 @@ import json
 
 
 class SendDingDing:
-    def sendDingDing(self):
+
+    def get_report_url(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+        return 'http://' + ip + ':8080' + '/job/jch-api/allure/'
+
+    def sendDingDing(self, case_num, success_num, error_num):
         url = 'https://oapi.dingtalk.com/robot/send?access_token=00eaa883c32dcc023a95b4ee84045659e2ddd8a8d5a98233cb0e2809d33c5f3d'
         program = {
             "msgtype": "text",
             "text": {
-                "content": "金城银行充值存入赎回提现接口已经通过测试。"
+                "金城银行接口自动化运行结果:" + "\n" + str(self.get_report_url()) + "\n" +
+                "运行测试用例:" + str(case_num) + "\n" + "成功:" + str(success_num) +
+                "\n" + "失败:" + str(error_num)
             },
         }
         headers = {'Content-Type': 'application/json'}
