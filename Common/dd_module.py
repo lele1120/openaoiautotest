ba5cf88cafd9209176afd2e47ddec27b1770416b
xuchen <357072695@qq.com>
2019-09-09 13:17:05 +0800

    滨海农商自动化

--- Common/dd_module.py
+++ Common/dd_module.py
@@ -2,26 +2,25 @@
 # @Time    : 2019/7/19 1:55 PM
 # @Author  : XuChen
 # @File    : dd_module.py
-import socket
-
 import requests
 import json
+import socket
 
 
 class SendDingDing:
-
     def get_report_url(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(('8.8.8.8', 80))
         ip = s.getsockname()[0]
-        return 'http://' + ip + ':8080' + '/job/jch-api/allure/'
+        return 'http://' + ip + ':8080' + '/job/滨海农商/allure/'
 
     def sendDingDing(self, case_num, success_num, error_num):
         url = 'https://oapi.dingtalk.com/robot/send?access_token=00eaa883c32dcc023a95b4ee84045659e2ddd8a8d5a98233cb0e2809d33c5f3d'
         program = {
             "msgtype": "text",
             "text": {
-                "滨海农商银行接口自动化运行结果:" + "\n" + str(self.get_report_url()) + "\n" +
+                "content":
+                "滨海农商接口自动化运行结果:" + "\n" + str(self.get_report_url()) + "\n" +
                 "运行测试用例:" + str(case_num) + "\n" + "成功:" + str(success_num) +
                 "\n" + "失败:" + str(error_num)
             },
